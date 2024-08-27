3be45d6c03e403590c888e60ede5df424fce3fd8
luscianaderose <luscianaderose@gmail.com>
2024-08-27 19:03:24 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'API Calendário OK'
+
+@app.route('/feriados/ano/<ano>')
+def feriados(ano):
+    resposta = requests.get('https://brasilapi.com.br/api/feriados/v1/' + ano)
+    if resposta.status_code == 200:
+        return resposta.json()
+    elif not resposta:
+        return []
+    else:
+        return f'Erro na requisição: {resposta.status_code}'
+    
+@app.route('/feriados/ano/<ano>/mes/<mes>')
+def feriados_mes(ano, mes):
+    resposta = requests.get('https://brasilapi.com.br/api/feriados/v1/' + ano)
+    if resposta.status_code == 200:
+        feriado_mes = []
+        for data in resposta.json():
+            #print('data', data)
+            #print('ano + mês:', ano + '-' + mes in data['date'])
+            if ano + '-' + mes in data['date']:
+                feriado_mes.append(data)
+        return feriado_mes
+        # return resposta.json()
+    else:
+        return f'Erro na requisição: {resposta.status_code}'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
+
+
+
