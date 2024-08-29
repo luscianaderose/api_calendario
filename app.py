729004f293f0fd7a703f2c8ef5cd47ede6a43372
luscianaderose <luscianaderose@gmail.com>
2024-08-29 20:57:49 -0300

    calendario tentando colocar feriados ainda sem exito

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import requests
 import calendar
+from datetime import date
 
 app = Flask(__name__)
 
@@ -36,10 +37,37 @@ def feriados_mes(ano, mes):
 @app.route('/calendario/ano/<ano>/mes/<mes>')
 def calendario(ano, mes):
     calendario = calendar.Calendar()
-    print(list(calendario.itermonthdates(int(ano), int(mes))))
-    return ''
+    cal_mes = list(calendario.itermonthdates(int(ano), int(mes)))
+    lista_dias_mes = []
+    dias_semana = ['S', 'T', 'Q', 'Q', 'S', 'S', 'D']
+    feriados = feriados_mes(ano, mes)
+    feriados_datas = []
+    for feriado in feriados:
+        feriados_datas.append(feriado['date'])
+    for dia_mes in cal_mes:
+        dia_data = ano + '-' + mes + '-' + str(dia_mes.day)
+        tem_feriado = dia_data in feriados_datas
+        if tem_feriado:
+            for feriado in feriados:
+                print(dia_data, feriado['date'], feriado['date'] == dia_data)
+                if feriado['date'] == dia_data:
+                    feriado = feriado['name']
+        else:
+            feriado = None
+        print(feriado)
+        dia_info = {
+            'dia_mes': dia_mes.day,
+            'dia_semana': dia_mes.weekday(),
+            'dia_semana_display': dias_semana[dia_mes.weekday()],
+            'tem_feriado': dia_data in feriados_datas,
+            'feriado': feriado,
+        }
+        lista_dias_mes.append(dia_info)
+    return lista_dias_mes
 
 
+# 'dia_semana': 'Mon',
+# 'dia_mes': 29,
 
 
 if __name__ == '__main__':
